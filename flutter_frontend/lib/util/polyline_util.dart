4f7c80f83db2b0cab81e778cce0a0a3883bcfbcc
Zulfikaryehya <zyehya007@gmail.com>
2025-02-28 17:16:36 +0200

    Markers have info when taped have option to see route to them and duration and distance and have the option to show all routes or route from location to stop

--- flutter_frontend/lib/util/polyline_util.dart
+++ flutter_frontend/lib/util/polyline_util.dart
@@ -1,11 +1,14 @@
 // polyline_utils.dart
 
 import 'dart:convert';
+import 'dart:math';
+import 'package:flutter/material.dart';
 import 'package:flutter_frontend/const.dart';
 import 'package:flutter_frontend/models/BusRoute_model.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter_polyline_points/flutter_polyline_points.dart';
+import 'package:uuid/uuid.dart';
 
 /// Fetches polyline points for a route between two locations using the provided waypoints.
 ///
@@ -168,4 +171,66 @@ busRouteToPolylineWaypoints() async {
   //PolylineWayPoint(location: '37.7859,-122.4364', stopOver: true),
   //PolylineWayPoint(location: '37.7963,-122.4574', stopOver: true)
   //],
+}
+
+Future<void> generatePolyLineFromPoints(
+  List<LatLng> pointCoordinates,
+  Map<PolylineId, Polyline> polylines,
+  Function(Map<PolylineId, Polyline>) updatePolylines,
+) async {
+  final PolylineId id = PolylineId(Uuid().v4()); // Generate a unique ID
+
+  final Random random = Random();
+  final List<Color> colors = [
+    Colors.red,
+    Colors.blue,
+    Colors.green,
+    Colors.yellow,
+    Colors.purple,
+    Colors.deepOrange,
+    // Add more colors as needed
+  ];
+  final Color randomColor = colors[random.nextInt(colors.length)];
+
+  final Polyline polyline = Polyline(
+    polylineId: id,
+    points: pointCoordinates,
+    width: 5,
+    color: randomColor,
+  );
+  polylines[id] = polyline; // Update the polylines map
+  updatePolylines(polylines); // Call function to update state in the main file
+}
+
+///polyline direction api for marker
+
+List<LatLng> decodePolyline(String encoded) {
+  List<LatLng> polylineCoordinates = [];
+  int index = 0, len = encoded.length;
+  int lat = 0, lng = 0;
+
+  while (index < len) {
+    int shift = 0, result = 0;
+    int b;
+    do {
+      b = encoded.codeUnitAt(index++) - 63;
+      result |= (b & 0x1F) << shift;
+      shift += 5;
+    } while (b >= 0x20);
+    int dlat = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+    lat += dlat;
+
+    shift = 0;
+    result = 0;
+    do {
+      b = encoded.codeUnitAt(index++) - 63;
+      result |= (b & 0x1F) << shift;
+      shift += 5;
+    } while (b >= 0x20);
+    int dlng = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+    lng += dlng;
+
+    polylineCoordinates.add(LatLng(lat / 1E5, lng / 1E5));
+  }
+  return polylineCoordinates;
 }
