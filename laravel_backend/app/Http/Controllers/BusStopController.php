4a6001db2bf0c2b4cbb274fffbf882cd15a15112
AliYahFouFi <aliyahfoufi122@gmail.com>
2025-03-04 23:30:51 +0200

    Add bus stop creation functionality with form and controller methods

--- laravel_backend/app/Http/Controllers/BusStopController.php
+++ laravel_backend/app/Http/Controllers/BusStopController.php
@@ -15,4 +15,23 @@ class BusStopController extends Controller
         $bus_stops = BusStop::all();
         return response()->json($bus_stops);
     }
+
+    public function create()
+    {
+        return view('bus_stops.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'address' => 'nullable|string|max:255',
+            'latitude' => 'required|numeric',
+            'longitude' => 'required|numeric',
+        ]);
+
+        BusStop::create($request->all());
+
+        return redirect()->route('bus_stops.create')->with('success', 'Bus Stop added successfully!');
+    }
 }
--- laravel_backend/routes/web.php
+++ laravel_backend/routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BusStopController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/bus-stops/create', [BusStopController::class, 'create'])->name('bus_stops.create');
+Route::post('/bus-stops/store', [BusStopController::class, 'store'])->name('bus_stops.store');
