25c5c75cebe6f5a650b77e857d8b95741fbaa300
Zulfikaryehya <zyehya007@gmail.com>
2025-03-13 00:16:44 +0200

    Set up firebase without using anything yet

--- flutter_frontend/lib/main.dart
+++ flutter_frontend/lib/main.dart
@@ -1,11 +1,20 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_frontend/firebase_options.dart';
 import 'package:flutter_frontend/providers/BusRouteProvider.dart';
 import 'package:flutter_frontend/providers/BusStopsProvider.dart';
 import 'package:flutter_frontend/providers/userLocationProvider.dart';
 import 'pages/HomePage.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+Future<void> main() async {
+  try {
+    WidgetsFlutterBinding.ensureInitialized();
+    await Firebase.initializeApp();
+    print("✅ Firebase initialized successfully!");
+  } catch (e) {
+    print("❌ Firebase initialization failed: $e");
+  }
   runApp(const MyApp());
 }
 
